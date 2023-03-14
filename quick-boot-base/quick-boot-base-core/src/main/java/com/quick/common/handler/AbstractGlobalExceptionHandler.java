6581cdc71065095f9ad3f13b0ce47bf6ff9ff6e7
csx-bill <1594805355@qq.com>
2023-03-14 11:00:01 +0800

    统一异常状态码

--- quick-boot-base/quick-boot-base-core/src/main/java/com/quick/common/handler/AbstractGlobalExceptionHandler.java
+++ quick-boot-base/quick-boot-base-core/src/main/java/com/quick/common/handler/AbstractGlobalExceptionHandler.java
@@ -18,35 +18,35 @@ import java.sql.SQLException;
 public abstract class AbstractGlobalExceptionHandler {
 
     @ExceptionHandler(NullPointerException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     public Result<?> nullPointerException(Exception ex){
         log.error("NullPointerException:{}", ex.getMessage());
         return Result.fail(ExceptionCode.NULL_POINT_EX.getCode(),ExceptionCode.NULL_POINT_EX.getMsg());
     }
 
     @ExceptionHandler(SQLException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     public Result<?> sqlException(SQLException ex) {
         log.error("SQLException:{}", ex.getMessage());
         return Result.fail(ExceptionCode.SQL_EX.getCode(),ExceptionCode.SQL_EX.getMsg() );
     }
 
     @ExceptionHandler(OAuth2Exception.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     public Result<?> exception(OAuth2Exception ex){
         log.error("OAuth2Exception:{}", ex.getMsg());
         return Result.fail(ex.getCode(),ex.getMsg());
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     public Result<?> usernameNotFoundException(UsernameNotFoundException ex){
         log.error("UsernameNotFoundException:{}", ex.getMsg());
         return Result.fail(ex.getCode(),ex.getMsg());
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     public Result<?> exception(Exception ex){
         log.error("Exception:{}", ex.getMessage());
         return Result.fail(ExceptionCode.SYSTEM_BUSY.getCode(),ExceptionCode.SYSTEM_BUSY.getMsg());
