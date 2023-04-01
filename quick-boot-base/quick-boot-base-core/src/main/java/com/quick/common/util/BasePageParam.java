6eb4499012cd976f8cff12eaba2f97e2ed5eb848
csx-bill <1594805355@qq.com>
2023-04-01 14:25:32 +0800

    统一分页请求参数

---
+++ quick-boot-base/quick-boot-base-core/src/main/java/com/quick/common/util/BasePageParam.java
@@ -0,0 +1,56 @@
+package com.quick.common.util;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基础分页
+ */
+@Getter
+@Setter
+@SuppressWarnings("ALL")
+@Accessors(chain = true)
+public class BasePageParam implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Schema(description = "当前页码")
+    private long pageNo;
+
+    @Schema(description = "每页显示数量")
+    private long pageSize;
+
+    @Schema(description = "排序的字段")
+    private String orderBy;
+
+    @Schema(description = "asc/desc")
+    private String orderDir;
+
+
+    @Schema(description = "排序的字段")
+    private List<OrderItem> orders;
+
+    public Page buildPage(){
+        Page page = new Page<>();
+        page.setCurrent(this.getPageNo());
+        page.setSize(this.getPageSize());
+        List<OrderItem> orders = new ArrayList<>();
+        if(!this.orderBy.isBlank()){
+            OrderItem orderItem = new OrderItem();
+            // 驼峰转下划线
+            orderItem.setColumn(StrUtil.toUnderlineCase(this.orderBy));
+            orderItem.setAsc(this.orderDir.equals("desc")?false:true);
+            orders.add(orderItem);
+        }
+        page.setOrders(orders);
+        return page;
+    }
+}
