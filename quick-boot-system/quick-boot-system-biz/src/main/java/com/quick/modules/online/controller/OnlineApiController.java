484b259b8ec67bb9e7daa04ed1806a47e41c4efb
csx-bill <1594805355@qq.com>
2023-07-21 16:17:16 +0800

    feat: APIJSON统一响应格式

--- quick-boot-system/quick-boot-system-biz/src/main/java/com/quick/modules/online/controller/OnlineApiController.java
+++ quick-boot-system/quick-boot-system-biz/src/main/java/com/quick/modules/online/controller/OnlineApiController.java
@@ -1,9 +1,12 @@
 package com.quick.modules.online.controller;
 
+import apijson.JSON;
+import apijson.JSONResponse;
 import apijson.RequestMethod;
 import apijson.framework.APIJSONController;
 import apijson.orm.Parser;
-import com.quick.modules.config.OnlineParser;
+import com.alibaba.fastjson.JSONObject;
+import com.quick.common.constant.CommonConstant;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -35,11 +38,12 @@ public class OnlineApiController extends APIJSONController<Long> {
      * @param session
      * @return
      */
-    @Operation(summary = "万能通用接口", description = "增删改查统一接口，这个一个接口可替代 7 个万能通用接口")
+    @Operation(summary = "在线通用接口", description = "增删改查统一接口，这个一个接口可替代 7 个万能通用接口")
     @PostMapping(value = "{method}")
     @Override
     public String crud(@PathVariable String method, @RequestBody String request, HttpSession session) {
-        return super.crud(method, request, session);
+        JSONObject res = JSON.parseObject(super.crud(method, request, session));
+        return processResponse(res);
     }
 
     /**
@@ -53,17 +57,28 @@ public class OnlineApiController extends APIJSONController<Long> {
      * @return
      */
     @PostMapping("{method}/{tag}")
-    @Operation(summary = "万能通用接口", description = "增删改查统一接口，这个一个接口可替代 7 个万能通用接口")
+    @Operation(summary = "在线通用接口", description = "增删改查统一接口，这个一个接口可替代 7 个万能通用接口")
     @Parameter(name = "method", description = "get,gets,head,heads,post,put,delete")
     @Parameter(name = "tag", description = "由后端Request表中指定")
     @Parameter(name = "params", description = "参数")
     @Override
     public String crudByTag(@PathVariable String method, @PathVariable String tag, @RequestParam Map<String, String> params, @RequestBody String request, HttpSession session) {
-        return super.crudByTag(method, tag, params, request, session);
+        JSONObject res = JSON.parseObject(super.crudByTag(method, tag, params, request, session));
+        return processResponse(res);
     }
 
-    @Override
-    public String parse(RequestMethod method, String request, HttpSession session) {
-        return new OnlineParser(method).setSession(session).setNeedVerify(false).parse(request);
+    /**
+     * 统一响应状态码字段
+     * @param res
+     * @return
+     */
+    private String processResponse(JSONObject res) {
+        if (res.getIntValue(JSONResponse.KEY_CODE) == JSONResponse.CODE_SUCCESS) {
+            res.put(JSONResponse.KEY_CODE, 0);
+            res.put("status", CommonConstant.SUCCESS_CODE);
+        } else {
+            res.put("status", res.getIntValue(JSONResponse.KEY_CODE));
+        }
+        return JSON.toJSONString(res);
     }
 }
