27d12e6d9cfe88f87148797e127cbeb598f6d1f2
csx-bill <1594805355@qq.com>
2023-07-27 23:50:10 +0800

    feat: 数据表 list 接口

--- quick-boot-system/quick-boot-system-biz/src/main/java/com/quick/modules/online/controller/AccessController.java
+++ quick-boot-system/quick-boot-system-biz/src/main/java/com/quick/modules/online/controller/AccessController.java
@@ -2,12 +2,16 @@ package com.quick.modules.online.controller;
 
 import com.quick.common.aspect.annotation.PreAuth;
 import com.quick.common.controller.SuperController;
+import com.quick.common.vo.Result;
 import com.quick.modules.online.entity.Access;
 import com.quick.modules.online.service.IAccessService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -16,5 +20,11 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "表权限配置")
 @PreAuth(replace = "Access:")
 public class AccessController extends SuperController<IAccessService, Access, String> {
+
+    @GetMapping(value = "/list")
+    @Operation(summary = "获取list", description = "获取list")
+    public Result<List<Access>> list() {
+        return Result.success(baseService.list());
+    }
 
 }
