e6f2edfbf37252cc302b333cd5fad2b33aa872fd
csx-bill <1594805355@qq.com>
2023-07-27 22:37:29 +0800

    feat: 删除无用代码

--- quick-boot-system/quick-boot-system-biz/src/main/java/com/quick/modules/system/service/ISysMenuSchemaService.java
+++
@@ -1,8 +0,0 @@
-package com.quick.modules.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.quick.modules.system.entity.SysMenuSchema;
-
-public interface ISysMenuSchemaService extends IService<SysMenuSchema> {
-    SysMenuSchema queryByPath(String path);
-}
