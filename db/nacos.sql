d2877b297dda0f94d94f0a2dc7c131ac1d3c1e27
csx-bill <1594805355@qq.com>
2023-07-27 23:54:41 +0800

    feat: db 文件更新

--- db/nacos.sql
+++ db/nacos.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 18/05/2023 17:54:46
+ Date: 27/07/2023 23:54:13
 */
 
 SET NAMES utf8mb4;
@@ -41,21 +41,21 @@ CREATE TABLE `config_info`  (
   `encrypted_data_key` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '秘钥',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_configinfo_datagrouptenant`(`data_id` ASC, `group_id` ASC, `tenant_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = 'config_info' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 47 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = 'config_info' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of config_info
 -- ----------------------------
-INSERT INTO `config_info` VALUES (2, 'quick-boot-gateway-biz.yml', 'DEFAULT_GROUP', 'server:\n  port: 9999\n\nspring:\n  cloud:\n    gateway:\n      routes:\n        - id: quick-boot-system-biz\n          uri: lb://quick-boot-system-biz\n          predicates:\n            - Path=/api/system/**\n          filters:\n            - StripPrefix=2\n        - id: quick-boot-oauth2-biz\n          uri: lb://quick-boot-oauth2-biz\n          predicates:\n            - Path=/api/auth/**\n          filters:\n            - StripPrefix=2\n\nknife4j:\n  # 聚合swagger文档\n  gateway:\n    enable: true\n    routes:\n      - name: 系统服务\n        url: /api/system/v3/api-docs?group=default\n        service-name: quick-boot-system-biz\n        order: 2\n      - name: 认证服务\n        url: /api/auth/v3/api-docs?group=default\n        service-name: quick-boot-oauth2-biz\n        order: 3', '5d7261018a9e61b822081df923b25759', '2023-01-24 14:05:23', '2023-03-10 17:35:51', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
+INSERT INTO `config_info` VALUES (2, 'quick-boot-gateway-biz.yml', 'DEFAULT_GROUP', 'server:\n  port: 9999\n\nspring:\n  cloud:\n    gateway:\n      routes:\n        - id: quick-boot-system-biz\n          uri: lb://quick-boot-system-biz\n          predicates:\n            - Path=/api/system/**\n          filters:\n            - StripPrefix=2\n        - id: quick-boot-oauth2-biz\n          uri: lb://quick-boot-oauth2-biz\n          predicates:\n            - Path=/api/auth/**\n          filters:\n            - StripPrefix=2\n\nknife4j:\n  # 聚合swagger文档\n  gateway:\n    #enable: true\n    enabled: true\n    routes:\n      - name: 系统服务\n        url: /api/system/v3/api-docs?group=default\n        service-name: quick-boot-system-biz\n        order: 2\n      - name: 认证服务\n        url: /api/auth/v3/api-docs?group=default\n        service-name: quick-boot-oauth2-biz\n        order: 3', '59653c881c28b8c3957e1618dc7db190', '2023-01-24 14:05:23', '2023-07-06 03:30:40', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 INSERT INTO `config_info` VALUES (3, 'quick-boot-oauth2-biz.yml', 'DEFAULT_GROUP', 'server:\n  port: 8082\n', 'f6e781ef3983031adce6986b746fe0ee', '2023-01-24 14:43:39', '2023-04-22 07:31:51', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 INSERT INTO `config_info` VALUES (4, 'redis.yml', 'DEFAULT_GROUP', 'spring:\n  cache:\n    type: redis\n  data:\n    redis:\n      host: 127.0.0.1\n      port: 6379\n      password: 123456', '2e2f4b1741b4d5338c2dd5a1e8ee72c3', '2023-01-24 16:49:35', '2023-05-04 08:11:06', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 INSERT INTO `config_info` VALUES (5, 'sa-token.yml', 'DEFAULT_GROUP', 'sa-token:\n  oauth2:\n    is-code: true\n    is-implicit: true\n    is-password: true\n    is-client: true\n  # token名称 (同时也是cookie名称)\n  token-name: Authorization\n  # token有效期，单位s 默认30天, -1代表永不过期\n  timeout: 2592000\n  # token临时有效期 (指定时间内无操作就视为token过期) 单位: 秒\n  activity-timeout: -1\n  # 是否允许同一账号并发登录 (为true时允许一起登录, 为false时新登录挤掉旧登录)\n  is-concurrent: true\n  # 在多人登录同一账号时，是否共用一个token (为true时所有登录共用一个token, 为false时每次登录新建一个token)\n  is-share: false\n  # token风格\n  token-style: random-64\n  # 是否输出操作日志\n  is-log: false', '91ba6d1397aa721893051d823201a389', '2023-01-24 16:53:20', '2023-04-22 14:46:16', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 INSERT INTO `config_info` VALUES (6, 'quick-boot-system-biz.yml', 'DEFAULT_GROUP', 'server:\n  port: 8080\n\nspring:\n  datasource:\n    dynamic:\n      primary: master #设置默认的数据源或者数据源组,默认值即为master\n      strict: false #严格匹配数据源,默认false. true未匹配到指定数据源时抛异常,false使用默认数据源\n      datasource:\n        master:\n          url: jdbc:p6spy:mysql://127.0.0.1:3306/quick-boot\n          username: root\n          password: 123456\n          driver-class-name: com.p6spy.engine.spy.P6SpyDriver\n', 'cb068869f5863bb007791d590dcf2948', '2023-01-24 17:13:11', '2023-04-22 07:31:34', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 INSERT INTO `config_info` VALUES (7, 'quick-boot-monitor-biz.yml', 'DEFAULT_GROUP', 'server:\n  port: 8081\n\n\n\n# 暴露监控端点\nmanagement:\n  endpoints:\n    web:\n      exposure:\n        include: \'*\'\n  endpoint:\n    health:\n      show-details: always', 'b138709410eea9f3adf52c162a967819', '2023-01-24 17:29:18', '2023-01-26 17:55:32', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
-INSERT INTO `config_info` VALUES (14, 'logging.yml', 'DEFAULT_GROUP', 'logging:\r\n  level:\r\n    root: info\r\n  pattern:\r\n    file: \'%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%thread] %F.%M - %msg%n\'\r\n    console: \'%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%thread] %F.%M - %msg%n\'\r\n  file:\r\n    path: logs\r\n    name: ${logging.file.path}/${spring.application.name}/root.log\r\n  logback:\r\n    rollingpolicy:\r\n      max-file-size: 50MB\r\n      max-history: 7', 'e35a48706c2ba165750cdcf341c47cfe', '2023-01-26 19:50:52', '2023-01-26 19:50:52', NULL, '0:0:0:0:0:0:0:1', '', 'dev', NULL, NULL, NULL, 'yaml', NULL, '');
+INSERT INTO `config_info` VALUES (14, 'logging.yml', 'DEFAULT_GROUP', 'logging:\n  level:\n    root: info\n  pattern:\n    file: \'%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [${spring.application.name:},%X{traceId:-},%X{spanId:-}] ==> [%thread] %F.%M - %msg%n\'\n    console: \'%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [${spring.application.name:},%X{traceId:-},%X{spanId:-}] ==> [%thread] %F.%M - %msg%n\'\n  file:\n    path: ./logs\n    name: ${logging.file.path}/${spring.application.name}/root.log\n  logback:\n    rollingpolicy:\n      max-file-size: 50MB\n      max-history: 7', '1b7f26401c67202206d19fef46440459', '2023-01-26 19:50:52', '2023-06-06 15:36:39', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 INSERT INTO `config_info` VALUES (15, 'monitor.yml', 'DEFAULT_GROUP', '# 暴露监控端点\r\nmanagement:\r\n  endpoints:\r\n    web:\r\n      exposure:\r\n        include: \'*\'\r\n  endpoint:\r\n    health:\r\n      show-details: always', 'ad12d0ced2301c74531e9719649721bf', '2023-01-26 20:13:39', '2023-01-26 20:13:39', NULL, '0:0:0:0:0:0:0:1', '', 'dev', NULL, NULL, NULL, 'yaml', NULL, '');
 INSERT INTO `config_info` VALUES (32, 'knife4j.yml', 'DEFAULT_GROUP', 'springdoc:\r\n  swagger-ui:\r\n    path: /swagger-ui.html\r\n    tags-sorter: alpha\r\n    operations-sorter: alpha\r\n  api-docs:\r\n    path: /v3/api-docs\r\n  group-configs:\r\n    - group: \'default\'\r\n      paths-to-match: \'/**\'\r\n      packages-to-scan: com.quick.modules\r\n\r\nknife4j:\r\n  enable: true\r\n  setting:\r\n    language: zh_cn\r\n    swagger-model-name: 实体类列表', 'd970aff43ea49736709338c0a7bd0cac', '2023-04-22 07:25:55', '2023-04-22 07:25:55', NULL, '0:0:0:0:0:0:0:1', '', 'dev', NULL, NULL, NULL, 'yaml', NULL, '');
-INSERT INTO `config_info` VALUES (33, 'mybatis-plus.yml', 'DEFAULT_GROUP', 'mybatis-plus:\r\n  global-config:\r\n    db-config:\r\n      logic-delete-field: delFlag\r\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\r\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\r\n      select-strategy: not_empty # 空字符查询\r\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml', '70f5291388f60ec752ffc96dcd56f0d7', '2023-04-22 07:26:42', '2023-04-22 07:26:42', NULL, '0:0:0:0:0:0:0:1', '', 'dev', NULL, NULL, NULL, 'yaml', NULL, '');
+INSERT INTO `config_info` VALUES (33, 'mybatis-plus.yml', 'DEFAULT_GROUP', 'mybatis-plus:\n  global-config:\n    db-config:\n      logic-delete-field: delFlag\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\n      select-strategy: not_empty # 空字符查询\n      column-format: \"`%s`\" # 关键词格式化\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml', 'fd8f10fe3662648221d99b32d12ea5bc', '2023-04-22 07:26:42', '2023-07-07 10:10:25', 'nacos', '0:0:0:0:0:0:0:1', '', 'dev', '', '', '', 'yaml', '', '');
 
 -- ----------------------------
 -- Table structure for config_info_aggr
@@ -72,7 +72,7 @@ CREATE TABLE `config_info_aggr`  (
   `tenant_id` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT '' COMMENT '租户字段',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_configinfoaggr_datagrouptenantdatum`(`data_id` ASC, `group_id` ASC, `tenant_id` ASC, `datum_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '增加租户字段' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '增加租户字段' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of config_info_aggr
@@ -98,7 +98,7 @@ CREATE TABLE `config_info_beta`  (
   `encrypted_data_key` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '秘钥',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_configinfobeta_datagrouptenant`(`data_id` ASC, `group_id` ASC, `tenant_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = 'config_info_beta' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = 'config_info_beta' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of config_info_beta
@@ -123,7 +123,7 @@ CREATE TABLE `config_info_tag`  (
   `src_ip` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT 'source ip',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_configinfotag_datagrouptenanttag`(`data_id` ASC, `group_id` ASC, `tenant_id` ASC, `tag_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = 'config_info_tag' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = 'config_info_tag' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of config_info_tag
@@ -167,7 +167,7 @@ CREATE TABLE `group_capacity`  (
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_group_id`(`group_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '集群、各Group容量信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '集群、各Group容量信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of group_capacity
@@ -196,17 +196,15 @@ CREATE TABLE `his_config_info`  (
   INDEX `idx_gmt_create`(`gmt_create` ASC) USING BTREE,
   INDEX `idx_gmt_modified`(`gmt_modified` ASC) USING BTREE,
   INDEX `idx_did`(`data_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '多租户改造' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 47 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '多租户改造' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of his_config_info
 -- ----------------------------
-INSERT INTO `his_config_info` VALUES (0, 32, 'knife4j.yml', 'DEFAULT_GROUP', '', 'springdoc:\r\n  swagger-ui:\r\n    path: /swagger-ui.html\r\n    tags-sorter: alpha\r\n    operations-sorter: alpha\r\n  api-docs:\r\n    path: /v3/api-docs\r\n  group-configs:\r\n    - group: \'default\'\r\n      paths-to-match: \'/**\'\r\n      packages-to-scan: com.quick.modules\r\n\r\nknife4j:\r\n  enable: true\r\n  setting:\r\n    language: zh_cn\r\n    swagger-model-name: 实体类列表', 'd970aff43ea49736709338c0a7bd0cac', '2023-04-22 15:25:54', '2023-04-22 07:25:55', NULL, '0:0:0:0:0:0:0:1', 'I', 'dev', '');
-INSERT INTO `his_config_info` VALUES (0, 33, 'mybatis-plus.yml', 'DEFAULT_GROUP', '', 'mybatis-plus:\r\n  global-config:\r\n    db-config:\r\n      logic-delete-field: delFlag\r\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\r\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\r\n      select-strategy: not_empty # 空字符查询\r\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml', '70f5291388f60ec752ffc96dcd56f0d7', '2023-04-22 15:26:41', '2023-04-22 07:26:42', NULL, '0:0:0:0:0:0:0:1', 'I', 'dev', '');
-INSERT INTO `his_config_info` VALUES (6, 34, 'quick-boot-system-biz.yml', 'DEFAULT_GROUP', '', 'server:\n  port: 8080\n\nspring:\n  datasource:\n    dynamic:\n      primary: master #设置默认的数据源或者数据源组,默认值即为master\n      strict: false #严格匹配数据源,默认false. true未匹配到指定数据源时抛异常,false使用默认数据源\n      datasource:\n        master:\n          url: jdbc:p6spy:mysql://127.0.0.1:3306/quick-boot\n          username: root\n          password: 123456\n          driver-class-name: com.p6spy.engine.spy.P6SpyDriver\n\nspringdoc:\n  swagger-ui:\n    path: /swagger-ui.html\n    tags-sorter: alpha\n    operations-sorter: alpha\n  api-docs:\n    path: /v3/api-docs\n  group-configs:\n    - group: \'default\'\n      paths-to-match: \'/**\'\n      packages-to-scan: com.quick.modules\n\nknife4j:\n  enable: true\n  setting:\n    language: zh_cn\n    swagger-model-name: 实体类列表\n\nmybatis-plus:\n  global-config:\n    db-config:\n      logic-delete-field: delFlag\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\n      select-strategy: not_empty # 空字符查询\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml\n', 'fd698ea0b635c36936a150c98d3c6fdb', '2023-04-22 15:31:33', '2023-04-22 07:31:34', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
-INSERT INTO `his_config_info` VALUES (3, 35, 'quick-boot-oauth2-biz.yml', 'DEFAULT_GROUP', '', 'server:\n  port: 8082\n\n\nspringdoc:\n  swagger-ui:\n    path: /swagger-ui.html\n    tags-sorter: alpha\n    operations-sorter: alpha\n  api-docs:\n    path: /v3/api-docs\n  group-configs:\n    - group: \'default\'\n      paths-to-match: \'/**\'\n      packages-to-scan: com.quick.modules\n\nknife4j:\n  enable: true\n  setting:\n    language: zh_cn\n    swagger-model-name: 实体类列表\n', '80f220967a39cfe2e72a8e7298566b39', '2023-04-22 15:31:51', '2023-04-22 07:31:51', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
-INSERT INTO `his_config_info` VALUES (5, 36, 'sa-token.yml', 'DEFAULT_GROUP', '', 'sa-token:\n  oauth2:\n    is-code: true\n    is-implicit: true\n    is-password: true\n    is-client: true\n  # token名称 (同时也是cookie名称)\n  token-name: Authorization\n  # token有效期，单位s 默认30天, -1代表永不过期\n  timeout: 2592000\n  # token临时有效期 (指定时间内无操作就视为token过期) 单位: 秒\n  activity-timeout: -1\n  # 是否允许同一账号并发登录 (为true时允许一起登录, 为false时新登录挤掉旧登录)\n  is-concurrent: true\n  # 在多人登录同一账号时，是否共用一个token (为true时所有登录共用一个token, 为false时每次登录新建一个token)\n  is-share: true\n  # token风格\n  token-style: random-64\n  # 是否输出操作日志\n  is-log: false', '17d211c8e521e2f3085c4e4b6ef67f14', '2023-04-22 22:46:16', '2023-04-22 14:46:16', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
-INSERT INTO `his_config_info` VALUES (4, 37, 'redis.yml', 'DEFAULT_GROUP', '', 'spring:\r\n  data:\r\n    redis:\r\n      host: 127.0.0.1\r\n      port: 6379\r\n      password: 123456', 'b70983a8f9caa431430f54f2c72d8908', '2023-05-04 16:11:06', '2023-05-04 08:11:06', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
+INSERT INTO `his_config_info` VALUES (2, 43, 'quick-boot-gateway-biz.yml', 'DEFAULT_GROUP', '', 'server:\n  port: 9999\n\nspring:\n  cloud:\n    gateway:\n      routes:\n        - id: quick-boot-system-biz\n          uri: lb://quick-boot-system-biz\n          predicates:\n            - Path=/api/system/**\n          filters:\n            - StripPrefix=2\n        - id: quick-boot-oauth2-biz\n          uri: lb://quick-boot-oauth2-biz\n          predicates:\n            - Path=/api/auth/**\n          filters:\n            - StripPrefix=2\n\nknife4j:\n  # 聚合swagger文档\n  gateway:\n    enable: true\n    routes:\n      - name: 系统服务\n        url: /api/system/v3/api-docs?group=default\n        service-name: quick-boot-system-biz\n        order: 2\n      - name: 认证服务\n        url: /api/auth/v3/api-docs?group=default\n        service-name: quick-boot-oauth2-biz\n        order: 3', '5d7261018a9e61b822081df923b25759', '2023-07-06 11:30:40', '2023-07-06 03:30:40', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
+INSERT INTO `his_config_info` VALUES (33, 44, 'mybatis-plus.yml', 'DEFAULT_GROUP', '', 'mybatis-plus:\r\n  global-config:\r\n    db-config:\r\n      logic-delete-field: delFlag\r\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\r\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\r\n      select-strategy: not_empty # 空字符查询\r\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml', '70f5291388f60ec752ffc96dcd56f0d7', '2023-07-07 18:07:03', '2023-07-07 10:07:03', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
+INSERT INTO `his_config_info` VALUES (33, 45, 'mybatis-plus.yml', 'DEFAULT_GROUP', '', 'mybatis-plus:\n  global-config:\n    db-config:\n      logic-delete-field: delFlag\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\n      select-strategy: not_empty # 空字符查询\n      column-format: \"`%s`\" # 关键词格式化\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml', 'fd8f10fe3662648221d99b32d12ea5bc', '2023-07-07 18:09:37', '2023-07-07 10:09:37', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
+INSERT INTO `his_config_info` VALUES (33, 46, 'mybatis-plus.yml', 'DEFAULT_GROUP', '', 'mybatis-plus:\n  global-config:\n    db-config:\n      logic-delete-field: delFlag\n      logic-delete-value: 1 # 逻辑已删除值(默认为 1)\n      logic-not-delete-value: 0 # 逻辑未删除值(默认为 0)\n      select-strategy: not_empty # 空字符查询\n      #column-format: \"`%s`\" # 关键词格式化\n  mapper-locations: classpath*:com/quick/modules/**/xml/*Mapper.xml', '0dfb4ff853a5fba64613ec8cd9bfa7c9', '2023-07-07 18:10:25', '2023-07-07 10:10:25', 'nacos', '0:0:0:0:0:0:0:1', 'U', 'dev', '');
 
 -- ----------------------------
 -- Table structure for permissions
@@ -255,7 +253,7 @@ CREATE TABLE `tenant_capacity`  (
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_tenant_id`(`tenant_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '租户容量信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '租户容量信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tenant_capacity
