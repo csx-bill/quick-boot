6c21c902c488fa5ad0d9068f9674945c41b3c829
csx-bill <1594805355@qq.com>
2023-11-20 11:00:33 +0800

    fix: 移除无用导入的包

--- quick-boot-online/quick-boot-online-controller/src/main/java/com/quick/online/controller/OnlineApiController.java
+++ quick-boot-online/quick-boot-online-controller/src/main/java/com/quick/online/controller/OnlineApiController.java
@@ -16,9 +16,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.Map;
-
 @Slf4j
 @RestController
 @RequestMapping("/Online/Api")
