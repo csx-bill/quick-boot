e9c3f52c2e10378f17dc21c02da7d69053bcf4aa
csx-bill <1594805355@qq.com>
2023-11-20 11:45:16 +0800

    fix: 1.移除无用代码 2.包扫描路径

--- quick-boot-oauth2/quick-boot-oauth2-start/src/main/java/com/quick/oauth2/QuickOAuth2Application.java
+++ quick-boot-oauth2/quick-boot-oauth2-start/src/main/java/com/quick/oauth2/QuickOAuth2Application.java
@@ -6,15 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @Slf4j
-@EnableFeignClients
+@EnableFeignClients(value ={"com.quick.*"})
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(value = {"com.quick.*"})
 public class QuickOAuth2Application {
     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext application = SpringApplication.run(QuickOAuth2Application.class, args);
