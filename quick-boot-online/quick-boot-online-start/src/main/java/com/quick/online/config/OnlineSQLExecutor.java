f4a75db63ebb9b5dc7a505ab42d6e1f177a0149c
csx-bill <1594805355@qq.com>
2023-11-21 16:28:56 +0800

    feat: 使用 SpringBeanUtils 获取bean

--- quick-boot-online/quick-boot-online-start/src/main/java/com/quick/online/config/OnlineSQLExecutor.java
+++ quick-boot-online/quick-boot-online-start/src/main/java/com/quick/online/config/OnlineSQLExecutor.java
@@ -3,7 +3,7 @@ package com.quick.online.config;
 import apijson.framework.APIJSONSQLExecutor;
 import apijson.orm.SQLConfig;
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
-import com.quick.online.QuickOnlineApplication;
+import com.quick.common.util.SpringBeanUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -17,7 +17,7 @@ public class OnlineSQLExecutor extends APIJSONSQLExecutor {
         String key = config.getDatasource() + "-" + config.getDatabase();
         Connection c = connectionMap.get(key);
         if (c == null || c.isClosed()) {
-            DataSource dataSource = QuickOnlineApplication.getApplicationContext().getBean(DataSource.class);
+            DataSource dataSource = SpringBeanUtils.getBean(DataSource.class);
             DynamicRoutingDataSource datasource = (DynamicRoutingDataSource) dataSource;
             DataSource ds = datasource.determineDataSource();
             connectionMap.put(key, ds == null ? null : ds.getConnection());
