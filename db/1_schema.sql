bc032581fa28cb88f641abe57e8cc622444b3a82
Bill <1594805355@qq.com>
2024-02-22 10:43:17 +0800

    feat: 增加 schema 创建脚本

---
+++ db/1_schema.sql
@@ -0,0 +1,19 @@
+-- nacos 库
+create database `nacos` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- quick-boot 库
+create database `quick-boot` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- quick-boot-online 库
+create database `quick-boot-online` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- quick-boot-flow 库
+create database `quick-boot-flow` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- quick-boot-jimureport 库
+create database `quick-boot-jimureport` default character set utf8mb4 collate utf8mb4_general_ci;
+
+
+
+
+
