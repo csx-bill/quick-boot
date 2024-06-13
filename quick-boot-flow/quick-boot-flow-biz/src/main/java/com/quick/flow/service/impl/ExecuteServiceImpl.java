1efd40c6fb18488d39fc869bf04578f7b130a4ce
Bill <1594805355@qq.com>
2024-06-13 18:22:03 +0800

    feat: 升级flow

--- quick-boot-flow/quick-boot-flow-biz/src/main/java/com/quick/flow/service/impl/ExecuteServiceImpl.java
+++ quick-boot-flow/quick-boot-flow-biz/src/main/java/com/quick/flow/service/impl/ExecuteServiceImpl.java
@@ -6,7 +6,7 @@ import com.quick.flow.service.IExecuteService;
 import com.warm.flow.core.dto.FlowParams;
 import com.warm.flow.core.entity.Instance;
 import com.warm.flow.core.service.InsService;
-import com.warm.tools.utils.IdUtils;
+import com.warm.flow.core.utils.IdUtils;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +25,18 @@ public class ExecuteServiceImpl implements IExecuteService {
 
     @Override
     public void startFlow(Flow flow, String tableName) {
-        Long id = IdUtils.nextId();
-        FlowParams flowParams = FlowParams.build().flowCode(tableName)
-                .createBy(StpUtil.getLoginIdAsString());
-//                .nickName(
-//                        //user.getUser().getNickName()
-//                        //StpUtil.get
-//                );
-        Instance instance = insService.start(String.valueOf(id), flowParams);
-        flow.setId(id);
-        flow.setInstanceId(instance.getId());
-        flow.setNodeCode(instance.getNodeCode());
-        flow.setNodeName(instance.getNodeName());
-        flow.setFlowStatus(instance.getFlowStatus());
+//        Long id = IdUtils.nextId();
+//        FlowParams flowParams = FlowParams.build().flowCode(tableName)
+//                .createBy(StpUtil.getLoginIdAsString());
+////                .nickName(
+////                        //user.getUser().getNickName()
+////                        //StpUtil.get
+////                );
+//        Instance instance = insService.start(String.valueOf(id), flowParams);
+//        flow.setId(id);
+//        flow.setInstanceId(instance.getId());
+//        flow.setNodeCode(instance.getNodeCode());
+//        flow.setNodeName(instance.getNodeName());
+//        flow.setFlowStatus(instance.getFlowStatus());
     }
 }
