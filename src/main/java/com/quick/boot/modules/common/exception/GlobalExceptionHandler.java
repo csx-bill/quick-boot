57d8b7c0a542dcb8a109f3d04d8ab67e41577fdd
dulerweil <dulerweil@uttec.com>
2025-12-02 08:46:09 +0800

    fix: 正确处理404

--- src/main/java/com/quick/boot/modules/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/quick/boot/modules/common/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 /**
  * 全局异常处理类
@@ -17,14 +18,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseBody
 public class GlobalExceptionHandler {
 
-
     @ExceptionHandler(CheckedException.class)
     @ResponseStatus(value = HttpStatus.OK)
     public R checkedException(CheckedException e) {
         log.error("校验异常信息 ex={}", e.getMessage(), e);
         return R.failed(e.getLocalizedMessage());
     }
-
 
     /**
      * 登录过期
@@ -36,6 +35,18 @@ public class GlobalExceptionHandler {
     public R notLoginException(NotLoginException e) {
         log.error("登录过期异常信息 ex={}", e.getMessage(), e);
         return R.failed(HttpStatus.UNAUTHORIZED.value(),e.getLocalizedMessage());
+    }
+
+    /**
+     * 文件找不到
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public R noResourceFoundException(NoResourceFoundException e) {
+        log.error("文件找不到异常信息 ex={}", e.getMessage(), e);
+        return R.failed(HttpStatus.NOT_FOUND.value(),e.getLocalizedMessage());
     }
 
     /**
